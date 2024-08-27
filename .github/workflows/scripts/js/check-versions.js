2eee86ebcd08c9224e8635267ec3404c466ddef0
Andrew Krokhmaliuk <andrii.krokhmaliuk@virtru.com>
2024-08-27 21:58:10 +0300

    debug

--- .github/workflows/scripts/js/check-versions.js
+++ .github/workflows/scripts/js/check-versions.js
@@ -1,8 +1,13 @@
+const path = require('path');
+
 module.exports = async ({ github }) => {
   console.log('github.workspace', github);
   const currentDirectory = process.cwd();
   console.log(`Current working directory: ${currentDirectory}`);
-  const manifestPath = '/chrome/manifest.json';
+  // const manifestPath = '/chrome/manifest.json';
+
+  const manifestPath = path.join(currentDirectory, 'chrome', 'manifest.json');
+
   const manifest = require(manifestPath);
   return null;
 }
